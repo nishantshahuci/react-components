fe6cf06f1f129ba3df9baa80f02f9fc79d389b3e
Nishant Shah <nishantshahuci@gmail.com>
2019-02-14 10:11:20 -0500

    Add faker module for avatar picture

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// import Faker
+import faker from "faker";
+
 // App component
 const App = () => {
     return (
         <div className="ui container comments">
             <div class="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src={faker.image.avatar()}/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
