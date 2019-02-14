e476837167811ca4dd1713084b11c23c7d95ad73
Nishant Shah <nishantshahuci@gmail.com>
2019-02-14 10:01:21 -0500

    Add Semantic-UI to project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+// import React
 import React from "react";
 import ReactDOM from "react-dom";
 
+// App component
 const App = () => {
   return <div>Hello World!</div>;
 };
 
+// render the App component
 ReactDOM.render(<App />, document.querySelector("#root"));
