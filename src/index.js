315c437884bfa1b2a62c0f8a67bfb5d4b1612f82
Nishant Shah <nishantshahuci@gmail.com>
2019-02-14 10:06:11 -0500

    Add base blog post template

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ import ReactDOM from "react-dom";
 
 // App component
 const App = () => {
-  return <div>Hello World!</div>;
+    return (
+        <div className="ui container comments">
+            <div class="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 // render the App component
