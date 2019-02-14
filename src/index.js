7b8fc79d972afe7a3496efa462f0a77dec3958b1
Nishant Shah <nishantshahuci@gmail.com>
2019-02-14 10:35:38 -0500

    Add new CommentDetail component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 // import React
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // import Faker
-import faker from "faker";
+import faker from 'faker';
+
+// import components
+import CommentDetail from './CommentDetail';
 
 // App component
 const App = () => {
     return (
         <div className="ui container comments">
-            <div class="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
         </div>
     );
 };
